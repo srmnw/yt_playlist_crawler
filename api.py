47eb071563e45de5658803572453cf732f06e0e7
srmnw <asarmanow@gmail.com>
2019-04-16 22:50:02 +0200

    convert api to class and fetch more than 50 items

--- api.py
+++ api.py
@@ -8,43 +8,89 @@ import googleapiclient.errors
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
 
-def api_public(developer_key, channel_id):
-    playlist_list = dict()
+class PrivateAPI:
+    def __init__(self, secret_file_path):
+        self.secret_file_path = secret_file_path
 
-    api_service_name = "youtube"
-    api_version = "v3"
-    DEVELOPER_KEY = developer_key
+        api_service_name = "youtube"
+        api_version = "v3"
 
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, developerKey = DEVELOPER_KEY)
+        # Get credentials and create an API client
+        flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+            self.secret_file_path, scopes)
+        credentials = flow.run_console()
+        self.youtube = googleapiclient.discovery.build(
+            api_service_name, api_version, credentials=credentials)
 
-    request = youtube.playlists().list(
-        part="snippet,contentDetails",
-        channelId=channel_id,
-        maxResults=25
-    )
-    response = request.execute()
-    #response = json.dumps(response, indent=4, sort_keys=True)
-    return response["items"]
+    def private_pl_list(self):
+        return_obj = list()
+        request = self.youtube.playlists().list(
+            part="snippet,contentDetails",
+            maxResults=50,
+            mine=True
+        )
+        while request is not None:
+            response = request.execute()
+            for item in response["items"]:
+                return_obj.append(item)
+            request = self.youtube.playlists().list_next(request, response)
+        return return_obj
 
-def api_private(secret_file_path):
+    def private_playlist(self, playlist_id):
+        return_obj = list()
 
-    api_service_name = "youtube"
-    api_version = "v3"
-    client_secrets_file = secret_file_path
+        request = self.youtube.playlistItems().list(
+            part="snippet,contentDetails",
+            maxResults=50,
+            playlistId=playlist_id
+        )
+        while request is not None:
+            response = request.execute()
+            for item in response["items"]:
+                return_obj.append(item)
+            request = self.youtube.playlistItems().list_next(request, response)
+        return return_obj
 
-    # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
-    credentials = flow.run_console()
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
 
-    request = youtube.playlists().list(
-        part="snippet,contentDetails",
-        maxResults=25,
-        mine=True
-    )
-    response = request.execute()
-    response = json.dumps(response, indent=4, sort_keys=True)
-    return response["items"]
+class PublicAPI:
+    def __init__(self, developer_key):
+        self.developer_key = developer_key
+
+    def public_pl_list(self, channel_id):
+        playlist_list = dict()
+
+        api_service_name = "youtube"
+        api_version = "v3"
+        DEVELOPER_KEY = self.developer_key
+
+        youtube = googleapiclient.discovery.build(
+            api_service_name, api_version, developerKey = DEVELOPER_KEY)
+
+        request = youtube.playlists().list(
+            part="snippet,contentDetails",
+            channelId=channel_id,
+            maxResults=50
+        )
+        while request is not None:
+            response = request.execute()
+            request = youtube.playlists().list_next(request, response)
+        return response["items"]
+
+    def public_playlist(self, playlist_id):
+        api_service_name = "youtube"
+        api_version = "v3"
+        DEVELOPER_KEY = self.developer_key
+
+        youtube = googleapiclient.discovery.build(
+            api_service_name, api_version, developerKey=DEVELOPER_KEY)
+
+        request = youtube.playlistItems().list(
+            part="snippet,contentDetails",
+            maxResults=50,
+            playlistId=playlist_id
+        )
+        response = request.execute()
+        return response["items"]
+
+
+
