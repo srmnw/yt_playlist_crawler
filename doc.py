473a4327f9d94224411cbbe3825551a02f1475cc
srmnw <asarmanow@gmail.com>
2019-04-17 02:37:31 +0200

    catch risky substrings in title

--- doc.py
+++ doc.py
@@ -5,6 +5,8 @@ OUTPUT_DIR = 'playlists'
 def create_doc(name, playlist):
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
+    if '/' in name:
+        name = name.replace('/', '_')
     with open('playlists/Playlist_' + name + '.txt', 'w+') as file:
         for item in playlist:
             file.write(item['snippet']['title'] + '\n')
